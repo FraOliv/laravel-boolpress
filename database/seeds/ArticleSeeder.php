f5708f5e2e0d1f45c2587129e1b6a6af9c3bde4d
FraOliv <olivotti.francesco@gmail.com>
2021-02-23 15:40:20 +0100

    created seeds for article

---
+++ database/seeds/ArticleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Article;
+
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $newArticle = new Article();
+            $newArticle->title = $faker->sentence(3);
+            $newArticle->subtitle = $faker->sentence(10);
+            $newArticle->author = $faker->name();
+            $newArticle->body = $faker->paragraph();
+            $newArticle->reading_time = $faker->randomDigitNotNull();
+            $newArticle->save();
+        }
+        //
+    }
+}
